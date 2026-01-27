a2373951bfeb357b64f341119b3c4d0c483e74bf
Fahama Abdul Rehman <23100230@lums.edu.pk>
2026-01-27 14:50:27 +0800

    CORS deployment issue fixed

--- backend/src/server.js
+++ backend/src/server.js
@@ -20,7 +20,10 @@ const PORT = process.env.PORT || 5000;
  * MIDDLEWARE
  */
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: [
+    'http://localhost:3000',
+    'https://paisa-smart-expense-tracker.vercel.app'
+  ],
   credentials: true
 }));
 
